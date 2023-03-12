55cb0b13996aed63f358544613e0c8e3267bdd20
Olajuwon Mubarak <olajuwonmubarak@hotmail.com>
2023-03-12 10:23:23 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,12 @@
-# change nginx open file limit
-exec { 'Change nginx limit':
-  command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
+# Sky is the limit, let's bring that limit higher
+
+exec {'replace':
   provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
